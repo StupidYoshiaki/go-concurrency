f6607271ac484d2c20114e57450e806cf8786361
StupidYoshiaki <nyanyap0418@gmail.com>
2025-04-01 18:45:10 +0900

    Fix

--- 4/main.go
+++ 4/main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"math/rand"
 	"sync"
+	"time"
 )
 
 func main() {
 	// adhocRestriction()
 	// lexicalRestriction()
-	unsafeRestriction()
+	// unsafeRestriction()
+	// cancelSignal()
+	cancelSignalWrite()
 }
 
 func adhocRestriction() {
@@ -71,4 +75,105 @@ func unsafeRestriction() {
 	go printData(&wg, data[3:])
 
 	wg.Wait()
+}
+
+func goroutineLeak() {
+	doWork := func(strings <-chan string) <-chan interface{} {
+		completed := make(chan interface{})
+		go func() {
+			defer fmt.Println("doWork exited.")
+			defer close(completed)
+			for s := range strings {
+				fmt.Println(s)
+			}
+		}()
+		return completed
+	}
+
+	doWork(nil)
+	fmt.Println("Done.")
+}
+
+func cancelSignal() {
+	doWork := func(
+		done <-chan interface{},
+		strings <-chan string,
+	) <-chan interface{} {
+		terminated := make(chan interface{})
+		go func() {
+			defer fmt.Println("doWork exited.")
+			defer close(terminated)
+			for {
+				select {
+				case s := <-strings:
+					fmt.Println(s)
+				case <-done:
+					return
+				}
+			}
+		}()
+		return terminated
+	}
+
+	done := make(chan interface{})
+	terminated := doWork(done, nil)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		fmt.Println("Canceling doWork goroutine...")
+		close(done)
+	}()
+
+	<-terminated
+	fmt.Println("Done.")
+}
+
+func goroutineLeakWrite() {
+	newRandStream := func() <-chan int {
+		randStream := make(chan int)
+		go func() {
+			defer fmt.Println("newRandStream closure exited.")
+			defer close(randStream)
+			for {
+				randStream <- rand.Int()
+			}
+		}()
+
+		return randStream
+	}
+
+	randStream := newRandStream()
+	fmt.Println("3 random ints")
+	for i := 1; i <= 3; i++ {
+		fmt.Printf("%d: %d\n", i, <-randStream)
+	}
+}
+
+func cancelSignalWrite() {
+	newRandStream := func(done <-chan interface{}) <-chan int {
+		randStream := make(chan int)
+		go func() {
+			defer fmt.Println("newRandStream closure exited.")
+			defer close(randStream)
+			for {
+				select {
+				case randStream <- rand.Int():
+				case <-done:
+					return
+				}
+			}
+		}()
+
+		return randStream
+	}
+
+	done := make(chan interface{})
+	randStream := newRandStream(done)
+	fmt.Println("3 random ints")
+	for i := 1; i <= 3; i++ {
+		fmt.Printf("%d: %d\n", i, <-randStream)
+	}
+	close(done)
+	// sleepしないと出力する前にメインゴールーチンが終わっちゃう
+	time.Sleep(1 * time.Second)
 }
